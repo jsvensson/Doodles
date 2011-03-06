ae40922c7b9e3b4e876aea52fe871db20844291f
Johan Svensson <johan@atomicplayboy.net>
2011-03-06 03:25:21 +0100

    Safe factorial

--- statistics.rb
+++ statistics.rb
@@ -1,8 +1,20 @@
+# Quick, dirty, unsafe factorial
 def fact(n)
   (1..n).inject(:*)
 end
 
-n = 5
-k = 3
+# Factorial with error checking
+def safe_fact(n)
+  if n < 1
+    raise "argument must be > 0"
+  elsif n == 1
+    1
+  else
+    n * safe_fact(n-1)
+  end
+end
 
-puts fact(n) / (fact(k) * fact(n-k))
+n = 8
+k = 7
+
+puts safe_fact(n) / (safe_fact(k) * safe_fact(n-k))
